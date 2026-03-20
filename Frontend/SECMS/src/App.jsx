d9c17c668ac0b8a54b2570d94e282ece069af47b
IT24103989 <it24103989@my.sliit.lk>
2026-03-21 00:32:36 +0530

    map path admindashboard and supplierdashboard

--- Frontend/SECMS/src/App.jsx
+++ Frontend/SECMS/src/App.jsx
@@ -4,6 +4,8 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
+import Supplierdashboard from './pages/Supplierdashboard.jsx';
+import Admindashboard from './pages/Admindashboard.jsx';
 
 function App() {
     return (
@@ -14,6 +16,8 @@ function App() {
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<Dashboard />} />
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/admindashboard" element={<Admindashboard/>} />
+                <Route path="/supplierdashboard" element={<Supplierdashboard />} />
             </Routes>
         </Router>
     );
