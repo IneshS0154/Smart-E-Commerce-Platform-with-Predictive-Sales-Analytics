fe342216ce3a2bc5438cfea5607206676b7fdf1d
IT24103989 <it24103989@my.sliit.lk>
2026-03-25 13:46:48 +0530

    update admin part

--- Backend/src/main/java/com/store/secms/controller/AdminController.java
+++ Backend/src/main/java/com/store/secms/controller/AdminController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-        import java.util.Optional;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin")
--- Backend/src/main/java/com/store/secms/entity/admin.java
+++ Backend/src/main/java/com/store/secms/entity/admin.java
@@ -1,11 +1,13 @@
 package com.store.secms.entity;
 
-
-
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "admins")
 public class admin {
