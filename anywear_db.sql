42107e3c1df0745709df93c0785ec005e5ded8f2
IneshS <71919137+IneshS0154@users.noreply.github.com>
2026-04-01 10:36:47 +0530

    Updated database query

--- anywear_db.sql
+++ anywear_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `anywear_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `anywear_db`;
 -- MySQL dump 10.13  Distrib 8.0.45, for macos15 (arm64)
 --
 -- Host: 127.0.0.1    Database: anywear_db
@@ -468,4 +470,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-04-01 10:20:29
+-- Dump completed on 2026-04-01 10:35:31
