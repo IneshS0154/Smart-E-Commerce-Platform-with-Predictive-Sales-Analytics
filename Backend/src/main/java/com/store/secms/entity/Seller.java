bdd9fb4ae9be4f2c5ffd397238899ec760db5599
IT24103989 <it24103989@my.sliit.lk>
2026-05-24 13:52:21 +0530

    remove seller product part

--- Backend/src/main/java/com/store/secms/entity/Seller.java
+++ Backend/src/main/java/com/store/secms/entity/Seller.java
@@ -1,11 +1,9 @@
 package com.store.secms.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -31,8 +29,4 @@ public class Seller {
 
     @Transient
     private String password;
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Product> products;
 }
